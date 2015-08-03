5f9eb05af4b1541ac578e9bf5476649a390a7094
Seth Wenchel <seth@wenchel.com>
2015-08-03 00:00:00 +0000

    version 0.1.0

--- R/tabe.R
+++ R/tabe.R
@@ -1,13 +1,14 @@
 # Copyright Seth Wenchel 2015
 
-#' @param  .data A tbl or soomething that can be coerced into one
+#' @param  .data A tbl or something that can be coerced into one
 #' @param ... conditions that will be passed to dplyr::filter
 #' @param false_fun A function or functional that will be applied to the data that doesn't pass the supplied filters (the scion)
-#' @param false_name optional, the name of the object to which the scion will be assigned. If specified, false_env must also be specified
-#' @param false_env option, the environment into which the scion will be assigned. If specified, false_name must also be specified
+#' @param false_name optional, the name of the object to which the scion will be assigned.
+#' @param false_env optional, the environment into which the scion will be assigned. If specified, false_name must also be specified.
+#'        If unspecified (default), scions will be placed into the internal package environment.
 #' @return A tbl whose rows have passed the stated conditions
-#' @details .data will be split into two chunks based on the conditions. The scion will be passed through false_fun and then either placed on
-#' the package's internal stack or assigned as specified by false_name and false_env.
+#' @details \code{.data} will be split into two chunks based on the conditions. The scion will be passed through \code{false_fun} and then either placed on
+#' the package's internal stack or assigned as specified by \code{false_name} and \code{false_env}.
 #' @examples
 #' library(dplyr)
 #' aframe <- data.frame(zed = runif(100))
@@ -29,27 +30,29 @@ scion <- function(.data, ..., false_fun, false_name, false_env){
     if(!missing(false_env))
       assign(false_name, falls, envir = false_env)
     else
-      stop("false_name specified but not false_env.  you must specify both or neither")
+      assign(false_name, falls, envir = .pkgenv)
   }
   else
     if(!missing(false_env))
-      stop("false_name specified but not false_env.  you must specify both or neither")
+      stop("false_env specified but not false_name.")
   .push(falls)
   return(tru)
 }
 
 
-#' @param .data A tbl or soomething that can be coerced into one
-#' @param data2 A tbl or soomething that can be coerced into one
-#' @param combine_fun optional, a function that will combbine two tbls such as full_join or bind_rows
+#' @param .data A tbl or something that can be coerced into one
+#' @param data2 A tbl or something that can be coerced into one
+#' @param combine_fun optional, A function that will combine two tbls such as full_join or bind_rows
 #' @description Graft one dataset onto another
 #' @details Graft requires two data objects.  The first must be provided by the user. The second can either be passed
 #' in or automatically pulled off of the package's internal stack of scions. These will be combined accoring to the following rules in order:
-#' * If either dataset has zero rows, the other dataset will be returned.
-#' * If combine_fun is specifed, combine_fun(.data, data2) will be called
-#' * If all column names match, a row bind will occur
-#' * If at least some column names match, a full join will occur
-#' * If both have the same number of rows a column bind will be performed
+#' \itemize{
+#'   \item If either dataset has zero rows, the other dataset will be returned.
+#'   \item If combine_fun is specifed, \code{combine_fun(.data, data2)} will be called
+#'   \item If all column names match, a row bind will occur
+#'   \item If at least some column names match, a full join will occur
+#'   \item If both have the same number of rows a column bind will be performed
+#'  }
 #' @return A single tbl object
 #' @export
 #' @author Seth Wenchel
@@ -62,9 +65,9 @@ graft <- function(.data, combine_fun, data2){
   if(missing(data2))
     data2 <- .pop()
 
-  if(nrow(.data)==0)
+  if(length(.data)==0)
     return(data2)
-  if(nrow(data2)==0)
+  if(length(data2)==0)
     return(.data)
   if(!missing(combine_fun))
     return(combine_fun(.data,data2))
@@ -81,10 +84,13 @@ graft <- function(.data, combine_fun, data2){
   # at least one column names matches <=> full_join
 }
 
+#' Remove all objects from the stack by deleting them from memory.
+#' @export
+clear_stack <- function(){
+  vars <-  ls(envir = .pkgenv)
+  rm(list = vars[which(substr(vars, 1,10)=="stack_obj_")], envir = .pkgenv)
+  assign("objects_on_stack", 0,.pkgenv)
+}
 
 
-# hark <- . %>% mutate(zed =0)
-#
-# aframe <- data.frame(zed = runif(100))
-#
-# aframe %>% scion(hark, zed >0.5) %>% mutate(zed=1) %>% graft
+
